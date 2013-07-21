9773515e1f05343ddf515f7799f99e59a983f75b
Joimer <joimpoke@gmail.com>
2013-07-21 18:37:08 +0200

    Add moves Add some move data, rename some moves in learnset so they're the same.

--- data/moves.js
+++ data/moves.js
@@ -1,18 +1,156 @@
 exports.BattleMovedex = {
-	"absorb": {
-		num: 71,
-		accuracy: 100,
-		basePower: 20,
-		category: "Special",
-		desc: "Deals damage to one adjacent target. The user recovers half of the HP lost by the target, rounded up. If Big Root is held by the user, the HP recovered is 1.3x normal, rounded half down.",
-		shortDesc: "User recovers 50% of the damage dealt.",
-		id: "absorb",
-		name: "Absorb",
-		pp: 25,
+	/**
+	 * First we list the DS-free cost moves that are static for all stages.
+	 */
+	// Fresh, In-Training
+	bubbles: {
+		name: "Bubbles",
+		basePower: 1,
+		desc: "The Digimon throws Bubbles to the enemy which deal puny damage.",
+		shortDesc: "Deals damage.",
+		ds: 0,
+		cd: 0,
 		priority: 0,
-		drain: [1,2],
 		secondary: false,
-		target: "normal",
-		type: "Grass"
-	}
+		target: "ranged",
+		type: "Neutral"
+	},
+	// Rookie
+	attackrookie: {
+		name: "Attack (Rookie)",
+		basePower: 10,
+		desc: "The Digimon attacks the enemy with its melee power.",
+		shortDesc: "Deals damage.",
+		ds: 0,
+		cd: 0,
+		priority: 0,
+		secondary: false,
+		target: "melee",
+		type: "Neutral"
+	},
+	// Champion
+	attackchampion: {
+		name: "Attack (Champion)",
+		basePower: 10,
+		desc: "The Digimon attacks the enemy with its melee power.",
+		shortDesc: "Deals damage.",
+		ds: 0,
+		cd: 0,
+		priority: 0,
+		secondary: false,
+		target: "melee",
+		type: "Neutral"
+	},
+	// Ultimate
+	attackultimate: {
+		name: "Attack (Ultimate)",
+		basePower: 10,
+		desc: "The Digimon attacks the enemy with its melee power.",
+		shortDesc: "Deals damage.",
+		ds: 0,
+		cd: 0,
+		priority: 0,
+		secondary: false,
+		target: "melee",
+		type: "Neutral"
+	},
+	// Mega
+	attackmega: {
+		name: "Attack (Mega)",
+		basePower: 10,
+		desc: "The Digimon attacks the enemy with its melee power.",
+		shortDesc: "Deals damage.",
+		ds: 0,
+		cd: 0,
+		priority: 0,
+		secondary: false,
+		target: "melee",
+		type: "Neutral"
+	},
+	// Burst
+	burstattack: {
+		name: "Burst Attack",
+		basePower: 10,
+		desc: "The Digimon attacks the enemy with its burst melee power.",
+		shortDesc: "Deals damage.",
+		ds: 0,
+		cd: 0,
+		priority: 0,
+		secondary: false,
+		target: "melee",
+		type: "Neutral"
+	},
+	// Jogress
+	doubleattack: {
+		name: "Double Attack",
+		basePower: 10,
+		desc: "The Digimon attacks the enemy with its melee power.",
+		shortDesc: "Deals damage.",
+		ds: 0,
+		cd: 0,
+		priority: 0,
+		secondary: false,
+		target: "melee",
+		type: "Neutral"
+	},
+	/**
+	 * Regular moves. Let's have fun putting them all!
+	 */
+	 babyclaw: {
+
+	 },
+	 babyflame: {
+
+	 },
+	 battlehawk: {
+
+	 },
+	 clawattack: {
+
+	 },
+	 clawuppercut: {
+
+	 },
+	 crossfire: {
+
+	 },
+	 hakasebo: {
+
+	 },
+	 hakasebou: {
+
+	 },
+	 pepperbreath: {
+		name: "Pepper Breath",
+		basePower: 100,
+		desc: "Agumon shoots fireballs to the enemy.",
+		shortDesc: "Shoots fireballs.",
+		ds: 12,
+		cd: 2,
+		priority: 0,
+		secondary: false,
+		target: "ranged",
+		type: "Fire"
+	 },
+	 sharpclaw: {
+
+	 },
+	 sharperclaw: {
+
+	 },
+	 surudoitsume: {
+
+	 },
+	 spitfireblast: {
+	 	name: "Spitfire Blast",
+		basePower: 275,
+		desc: "Agumon shoots flames to his enemies, burning and hurting them.",
+		shortDesc: "Shoots flame from Agumon's mouth.",
+		ds: 19,
+		cd: 4,
+		priority: 0,
+		secondary: false,
+		target: "ranged",
+		type: "Fire"
+	 },
 };
